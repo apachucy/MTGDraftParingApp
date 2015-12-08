f862b0600ed997b6a28dbb15f7c6ce329c23f2c6
Arkadiusz Pachucy <pachucek@gmail.com>
2015-12-08 16:52:24 +0100

    Update MainScreen of application to utilize designLibrary and TextInput

--- MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/view/fragments/GameMenuFragment.java
+++ MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/view/fragments/GameMenuFragment.java
@@ -5,11 +5,11 @@ import android.content.Intent;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -40,43 +40,44 @@ public class GameMenuFragment extends BaseFragment {
     private IPlayerList mPlayerNameList;
     private ArrayAdapter<String> mListAdapter;
 
-    @Bind(R.id.init_playerNameEditText)
-    EditText mPlayerNameEditText;
-    @Bind(R.id.init_roundsEditText)
-    EditText mRoundsEditText;
+
     @Bind(R.id.init_playerList)
     ListView mPlayerList;
+
+    @Bind(R.id.init_playerNameTextInput)
+    TextInputLayout mPlayerNameTextInput;
+
+    @Bind(R.id.init_roundsTextInput)
+    TextInputLayout mRoundsTextInput;
 
     @OnClick(R.id.init_addPlayerButton)
     void onAddPlayerClick(View view) {
         // add new player
-        if (!ValidationHelper.isEditTextEmpty(mPlayerNameEditText,
-                getString(R.string.warning_empty_field),
-                mWarningDrawable) && !isNameAddedBefore(mPlayerNameEditText.getText()
+        if (!ValidationHelper.isTextInputFieldEmpty(mPlayerNameTextInput,
+                getString(R.string.warning_empty_field)) && !isNameAddedBefore(mPlayerNameTextInput.getEditText().getText()
                 .toString())) {
-            mPlayerNameList.getPlayerList().add(mPlayerNameEditText.getText()
+            mPlayerNameList.getPlayerList().add(mPlayerNameTextInput.getEditText().getText()
                     .toString());
-            mPlayerNameEditText.setText("");
+            mPlayerNameTextInput.getEditText().setText("");
             mListAdapter.notifyDataSetChanged();
 
-        } else if (isNameAddedBefore(mPlayerNameEditText.getText()
+        } else if (isNameAddedBefore(mPlayerNameTextInput.getEditText().getText()
                 .toString())) {
-            mPlayerNameEditText.setError(getResources().getString(R.string.warning_player_name_added), mWarningDrawable);
+            mPlayerNameTextInput.setError(getResources().getString(R.string.warning_player_name_added));
         }
     }
 
     @OnClick(R.id.init_roundsButton)
     void onStartGameClick(View view) {
-        if (!ValidationHelper.isEditTextEmpty(mRoundsEditText,
-                getString(R.string.warning_empty_field),
-                mWarningDrawable)) {
+        if (!ValidationHelper.isTextInputFieldEmpty(mRoundsTextInput,
+                getString(R.string.warning_empty_field))) {
             if (mPlayerNameList.getPlayerList().isEmpty() || mPlayerNameList.getPlayerList().size() < 2) {
                 Toast.makeText(mActivity,
                         getString(R.string.warning_need_players),
                         Toast.LENGTH_LONG).show();
                 // if number of player where bigger than
                 // round ask user to change it
-            } else if (Integer.parseInt(mRoundsEditText.getText()
+            } else if (Integer.parseInt(mRoundsTextInput.getEditText().getText()
                     .toString()) >= mPlayerNameList.getPlayerList().size()) {
                 if (mWarningDialogFragment == null) {
                     mWarningDialogFragment = CustomDialogFragment
@@ -91,7 +92,7 @@ public class GameMenuFragment extends BaseFragment {
             } else {
                 mStartGameDialogFragment = CustomDialogFragment.newInstance(
                         getString(R.string.dialog_start_title),
-                        getString(R.string.dialog_start_message, mPlayerNameList.getPlayerList().size(), Integer.parseInt(mRoundsEditText.getText().toString())),
+                        getString(R.string.dialog_start_message, mPlayerNameList.getPlayerList().size(), Integer.parseInt(mRoundsTextInput.getEditText().getText().toString())),
                         getString(R.string.start_game));
 
                 mStartGameDialogFragment.show(getFragmentManager(),
@@ -125,7 +126,10 @@ public class GameMenuFragment extends BaseFragment {
         mListAdapter = new ArrayAdapter<String>(mActivity, R.layout.row_player_name,
                 mPlayerNameList.getPlayerList());
 
-
+        mPlayerNameTextInput.setHint(getString(R.string.hint_player_name));
+        mRoundsTextInput.setHint(getString(R.string.hint_rounds));
+        mRoundsTextInput.setErrorEnabled(true);
+        mPlayerNameTextInput.setErrorEnabled(true);
         View header = inflater.inflate(R.layout.header_names, null);
 
         mPlayerList.addHeaderView(header);
@@ -144,11 +148,11 @@ public class GameMenuFragment extends BaseFragment {
                 Intent intent = null;
                 IAlgorithmConfigure algorithmConfigure = (IAlgorithmConfigure) mActivity.getApplication();
                 if (SettingsPreferencesFactory.getInstance().areManualParings()) {
-                    algorithmConfigure.setAlgorithm(new ManualParingAlgorithm(mPlayerNameList.getPlayerList(), Integer.parseInt(mRoundsEditText.getText().toString())));
+                    algorithmConfigure.setAlgorithm(new ManualParingAlgorithm(mPlayerNameList.getPlayerList(), Integer.parseInt(mRoundsTextInput.getEditText().getText().toString())));
                     intent = new Intent(mActivity,
                             ManualPlayerPairingActivity.class);
                 } else {
-                    algorithmConfigure.setAlgorithm(new ParingAlgorithm(mPlayerNameList.getPlayerList(), Integer.parseInt(mRoundsEditText.getText().toString())));
+                    algorithmConfigure.setAlgorithm(new ParingAlgorithm(mPlayerNameList.getPlayerList(), Integer.parseInt(mRoundsTextInput.getEditText().getText().toString())));
                     intent = new Intent(mActivity,
                             ParingDashboardActivity.class);
 
