cc5263f2f6591bae5484e0ebccedc914a637a8ac
Arkadiusz Pachucy <arkadiusz.pachucy@payu.com>
2017-10-06 15:56:51 +0200

    Add adapter for single player

---
+++ MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/view/adapters/AddPlayerAdapter.java
@@ -0,0 +1,54 @@
+package unii.draft.mtg.parings.view.adapters;
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import unii.draft.mtg.parings.R;
+
+public class AddPlayerAdapter extends RecyclerView.Adapter<AddPlayerAdapter.ViewHolder> {
+
+    private List<String> mPlayerNames;
+
+    public AddPlayerAdapter(List<String> playerNames) {
+        mPlayerNames = playerNames;
+    }
+
+    @NonNull
+    @Override
+    public AddPlayerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_player_name, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(AddPlayerAdapter.ViewHolder holder, int position) {
+        String playerName = mPlayerNames.get(position);
+        holder.mPlayerName.setText(playerName);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPlayerNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @Nullable
+        @Bind(R.id.row_player)
+        TextView mPlayerName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
