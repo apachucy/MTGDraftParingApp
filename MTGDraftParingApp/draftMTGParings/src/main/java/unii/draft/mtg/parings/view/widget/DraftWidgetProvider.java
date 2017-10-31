de7b12d2fd8fb69e8cbc3b5b66fd96aa9be442ad
Arkadiusz Pachucy <arkadiusz.pachucy@payu.com>
2017-10-31 12:14:44 +0100

    cleaning widget feature

--- MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/view/widget/DraftWidgetProvider.java
+++ MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/view/widget/DraftWidgetProvider.java
@@ -2,14 +2,15 @@ package unii.draft.mtg.parings.view.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 import unii.draft.mtg.parings.R;
-import unii.draft.mtg.parings.view.widget.WidgetViewModel;
+
+//TODO: prepare better text
 
 /**
  * Implementation of App Widget functionality.
@@ -19,9 +20,51 @@ public class DraftWidgetProvider extends AppWidgetProvider {
     private static WidgetViewModel widgetViewModel = null;
 
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
 
+        if (!intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+            return;
+        }
+
+        Bundle bundle = intent.getExtras();
+        if (!bundle.containsKey(BUNDLE_EXTRA)) {
+            return;
+        }
+        updateAppWidget(context, bundle);
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private static void updateAppWidget(Context context, Bundle bundle) {
+        widgetViewModel = (WidgetViewModel) bundle.getSerializable(BUNDLE_EXTRA);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_draft);
+        views.setImageViewResource(R.id.widget_icon, R.drawable.ic_launcher);
+        String widgetData;
+        if (widgetViewModel != null) {
+            widgetData = context.getString(R.string.widget_content, widgetViewModel.getCurrentRound(), widgetViewModel.getWinningPlayer());
+        } else {
+            widgetData = context.getString(R.string.widget_empty_content);
+        }
+        views.setTextViewText(R.id.widget_text, widgetData);
+        // This time we dont have widgetId. Reaching our widget with that way.
+        ComponentName appWidget = new ComponentName(context, DraftWidgetProvider.class);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidget, views);
+    }
+
+    private static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                        int appWidgetId) {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_draft);
 
@@ -36,38 +79,5 @@ public class DraftWidgetProvider extends AppWidgetProvider {
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
-
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (!intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-            return;
-        }
-
-        Bundle bundle = intent.getExtras();
-        if (!bundle.containsKey(BUNDLE_EXTRA)) {
-            return;
-        }
-        widgetViewModel = (WidgetViewModel) bundle.getSerializable(BUNDLE_EXTRA);
-        super.onReceive(context, intent);
-    }
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
-
-   // @Override
-  //  public void onEnabled(Context context) {
-  //      // Enter relevant functionality for when the first widget is created
-  //  }
-
-  //  @Override
-   // public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
- //   }
 }
 
