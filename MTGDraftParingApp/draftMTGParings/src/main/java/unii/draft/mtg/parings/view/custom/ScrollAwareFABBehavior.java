6164dfb95d49721a5340ffe271a5801f98391d32
Arkadiusz Pachucy <arkadiusz.pachucy@payu.com>
2018-01-25 11:49:40 +0100

    Added FAB behaviour for application

---
+++ MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/view/custom/ScrollAwareFABBehavior.java
@@ -0,0 +1,41 @@
+package unii.draft.mtg.parings.view.custom;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFABBehavior(Context context, AttributeSet attributeSet) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+
+}
