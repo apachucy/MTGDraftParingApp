d49b8d173a0118554ebf49a2bf6878240826a311
Arkadiusz Pachucy <arkadiusz.pachucy@payu.com>
2018-07-18 09:34:54 +0200

    Fixing issues with crashing app on Toolbar on Scoreboard Activity, Fix "position" after draft for playesr Change icons for History Removing unused strings

--- MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/util/helper/DatabaseHelper.java
+++ MTGDraftParingApp/draftMTGParings/src/main/java/unii/draft/mtg/parings/util/helper/DatabaseHelper.java
@@ -146,7 +146,7 @@ public class DatabaseHelper implements IDatabaseHelper {
             int nextIndex = playerIndex + 1;
             Player nextPlayer = nextIndex < playerDraftList.size() ? playerDraftList.get(nextIndex) : null;
             if (nextPlayer == null || !(comparator.compare(player, nextPlayer) == EQUAL)) {
-            position++;
+                position = nextIndex;
         }
         }
 
